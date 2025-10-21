64b6ea146f2b5a4261471cf3f00494d532facd45
Andrew Doyle <atdoyle@asu.edu>
2025-10-21 16:05:55 -0700

    File Creation
    Created qnn

---
+++ QNN/QNN.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 21 15:31:51 2025
+
+@author: Andrew
+"""
+
+import torch.nn as nn
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Oct 21 15:13:19 2025
 
-@author: maste
+@author: Andrew
 """
 
 import Graph.Graph as g
